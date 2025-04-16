f4a7d770cf256ee75b6946f5007967f025d7fac9
amo-ladoja <54277725+amo-ladoja@users.noreply.github.com>
2025-04-16 08:16:07 -0400

    update cors orging

--- backend/file_upload_project/file_upload_project/settings.py
+++ backend/file_upload_project/file_upload_project/settings.py
@@ -74,7 +74,7 @@ MEDIA_ROOT = '/var/www/preppad/backend/media/'
 
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
-    '[https://prepadai.eastus.cloudapp.azure.com](https://prepadai.eastus.cloudapp.azure.com)',
+    'https://prepadai.eastus.cloudapp.azure.com',
     'http://localhost:8000',
 ]
 CORS_ALLOW_ALL_ORIGINS = False
